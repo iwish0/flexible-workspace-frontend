b280520f4b301aeb930ebe670d06500651caee33
CEDRIC BRESSON <c.bresson@proxiad.com>
2023-06-23 11:34:30 +0200

    modify link on navbar

--- src/shared/constants/navbar.constant.ts
+++ src/shared/constants/navbar.constant.ts
@@ -1,23 +1,8 @@
 import { Route } from './route.constant';
 
-export const DESK_BOOKING_KEY: string = Route.DESK_BOOKING;
-export const DROPDOWN_MENU_ITEMS: { label: string, key: string, description: string, icon: string }[] = [
-    {
-        label: 'Place de bureau',
-        key: DESK_BOOKING_KEY,
-        description: '',
-        icon: ''
-    },
-    {
-        label: 'Salle',
-        key: Route.ROOM_BOOKING,
-        description: 'Salle de réunion, bureau individuel...',
-        icon: ''
-    }
-];
-
-export const collapseItems: { label: string, route: string }[] = [
-    { label: 'Réserver un bureau', route: Route.DESK_BOOKING },
-    { label: 'Réserver une salle', route: Route.ROOM_BOOKING },
-    { label: 'Historique', route: Route.USER_BOOKING_HISTORY },
+export const NAVBAR_ITEMS: { longLabel: string; shortlabel: string, route: string }[] = [
+    { shortlabel: 'Bureau', longLabel: 'Réserver un bureau', route: Route.DESK_BOOKING },
+    { shortlabel: 'Salle', longLabel: 'Réserver une salle', route: Route.ROOM_BOOKING },
+    { shortlabel: 'Historique', longLabel: 'Historique', route: Route.USER_BOOKING_HISTORY },
+    { shortlabel: 'Login', longLabel: 'Login', route: '' },
 ];
--- src/shared/models/ihm/navigation.model.ts
+++
@@ -1,4 +0,0 @@
-export type RouteStatus = {
-    isRouteDeskBooking: boolean;
-    isRouteUserBookingHistory: boolean;
-}
