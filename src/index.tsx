132ed96068bb46dfddbf77307e77515316deacd9
CEDRIC BRESSON <c.bresson@proxiad.com>
2023-06-30 14:08:45 +0200

    add authentifcation configuration and provider

--- src/index.tsx
+++ src/index.tsx
@@ -9,21 +9,28 @@ import { theme } from './theme';
 import { App } from './App';
 import React from 'react';
 import './index.css';
+// Scripts used for the authentication
+import { PublicClientApplication } from '@azure/msal-browser';
+import { MsalProvider } from '@azure/msal-react';
+import { msalConfig } from './authConfig';
 
 registerLocale(LOCALE, fr);
-
+const msalInstance = new PublicClientApplication(msalConfig);
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
 root.render(
   <React.StrictMode>
-    <NextUIProvider theme={theme}>
-      <IconlyProvider
-        set='light'
-        primaryColor='black'
-        secondaryColor='white'
-        stroke='bold'
-        size='xlarge'
-      />
-      <App />
-    </NextUIProvider>
+    <MsalProvider instance={msalInstance}>
+      <NextUIProvider theme={theme}>
+        <IconlyProvider
+          set='light'
+          primaryColor='black'
+          secondaryColor='white'
+          stroke='bold'
+          size='xlarge'
+        />
+        <App />
+      </NextUIProvider>
+    </MsalProvider>
   </React.StrictMode>
 );
